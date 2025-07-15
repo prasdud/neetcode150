9cb4d32ec1bcb41e3d089a58557d283f80757687
prasdud <prasdudwastaken@gmail.com>
2025-07-15 22:31:22 +0530

    s

--- misc/operator_overloading.cpp
+++ misc/operator_overloading.cpp
@@ -6,9 +6,60 @@ using namespace std;
 #define endl '\n'
 #define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define all(x) (x).begin(), (x).end()
+#define PI 3.14159
+
+class operator_overloading
+{
+    private:
+    float radius;
+    float area;
+
+        /* data */
+    public:
+    void setRadius(float val){
+        radius = val;
+    }
+
+    float getRadius(){
+        cout << radius;
+        return radius;
+    }
+
+    void setArea(float val){
+        area = val;
+    }
+
+    float getArea(){
+        cout << area;
+        return area;
+    }
+
+    float calcArea(){
+        return PI*radius*radius;
+    }
+
+    bool operator==(operator_overloading const&obj){
+        operator_overloading res;
+        return this ->area == obj.area;
+    }
+};
+
+
 
 void solve() {
     // Your code here
+    operator_overloading op1;
+    op1.setRadius(2.14);
+    op1.setArea(op1.calcArea());
+    float res = op1.getArea();
+    cout << "The area is "<<res<<endl;
+
+    operator_overloading op2;
+    op2.setRadius(2.11);
+    op2.setArea(op2.calcArea());
+    bool result = op1==op2;
+    cout <<"The both areas are equal = "<< result<<endl;
+
 }
 
 int32_t main() {
