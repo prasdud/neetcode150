5109852abf84c15c2e8c570f87ffbb3ac1ee2d41
prasdud <prasdudwastaken@gmail.com>
2025-07-15 22:43:27 +0530

    singleton

--- misc/singleton_design_pattern.cpp
+++ misc/singleton_design_pattern.cpp
@@ -7,9 +7,47 @@ using namespace std;
 #define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define all(x) (x).begin(), (x).end()
 
+
+class singleton_design_pattern
+{
+    private:
+    
+    singleton_design_pattern(){}
+    static singleton_design_pattern* instance;
+        /* data */
+    
+    public:
+    void test() {
+        cout << "This should not print when calling directly" << endl;
+    }
+
+    static singleton_design_pattern* getInstance(){
+        if(instance == nullptr)
+            instance = new singleton_design_pattern();
+        return instance;
+    }
+};
+
+singleton_design_pattern* singleton_design_pattern::instance = nullptr;
+
 void solve() {
-    // Your code here
+    singleton_design_pattern* a = singleton_design_pattern::getInstance();
+    singleton_design_pattern* b = singleton_design_pattern::getInstance();
+
+    //these should fail
+    //singleton_design_pattern* s2 = new singleton_design_pattern();
+    //singleton_design_pattern s1;
+
+    if (a == b) {
+        cout << "singleton works" << endl;
+    } else {
+        cout << "different instances singleton fail" << endl;
+    }
+
+    a->test();
+    b->test();
 }
+
 
 int32_t main() {
     fast_io;
