46a0c02fcb5b940ab0fcf676df00c393f954007d
prasdud <prasdudwastaken@gmail.com>
2025-07-15 22:54:27 +0530

    reverse linked list

--- misc/rev_linked_list.cpp
+++ misc/rev_linked_list.cpp
@@ -7,7 +7,68 @@ using namespace std;
 #define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define all(x) (x).begin(), (x).end()
 
+struct Node {
+    int data;
+    Node* next;
+};
+
+void insert(Node*& head, int value) {
+    Node* newNode = new Node{value, nullptr};
+    
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+    
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    
+    temp->next = newNode;
+}
+
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void reverseList(Node* &head){
+    Node* prev = nullptr;
+    Node* current = head;
+    Node* next = nullptr;
+
+    while (current != nullptr)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        /* code */
+
+    }
+    head = prev;
+    
+}
+
 void solve() {
+    Node* head = nullptr;
+    insert(head, 23);
+    insert(head, 212);
+    insert(head, 312);
+    insert(head, 231);
+    insert(head, 0);
+    insert(head, 99);
+
+    cout << "original list"<<endl;
+    printList(head);
+    reverseList(head);
+    cout << "reversed list"<<endl;
+    printList(head);
+    
     // Your code here
 }
 
